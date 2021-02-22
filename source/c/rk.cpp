f66dfde47bcd5bf52bbf8ac2ef59bd696ca6175c
nnarenraju <nnarenraju@gmail.com>
2021-02-22 17:55:47 +0530

    Essential sub-directories and init code

---
+++ source/c/rk.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* param prep and transfer to rk4 */
+	
+
+	return 0;
+}
+
+int rk4()
+{
+	int a, gamma, 
+
+
+}
---
+++ source/c/rk.h
@@ -0,0 +1,14 @@
+typedef struct _INPUT
+{
+  int run;
+  double mu_mol;
+  double Mwd;
+  double Rwd;
+  double Bwd;
+  double Omega;
+  double Mdot;
+  double TA;
+  double LrA;
+  double rA;
+  double dudxA;
+} INPUT;
